e1ba474ade1c1f2f34481b1b5a1e45ec39452255
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-09-27 08:20:15 -0700

    refactor: move the specialized parsing fns

--- internal/parser/parse_expression_fns.go
+++ internal/parser/parse_expression_fns.go
@@ -1,8 +1,15 @@
 package parser
 
-import "github.com/w-h-a/interpreter/internal/parser/ast"
+import (
+	"github.com/w-h-a/interpreter/internal/parser/ast"
+	"github.com/w-h-a/interpreter/internal/parser/ast/expression"
+)
 
 type (
-	parsePrefixExpression func() ast.Expression
-	parseInfixExpression  func(ast.Expression) ast.Expression
+	parsePrefixExpression func(*Parser) ast.Expression
+	parseInfixExpression  func(*Parser, ast.Expression) ast.Expression
 )
+
+func parseIdentifier(p *Parser) ast.Expression {
+	return &expression.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
